89ca05947bc4aab6073e4ef37e632f3b1f75ab49
Alex <a.caignard@hotmail.fr>
2021-02-27 19:02:11 +0100

    10. refs et le DOM

--- 10-refs/app.jsx
+++ 10-refs/app.jsx
@@ -1,3 +1,27 @@
+// Exemple 2.2 : Pour faire descendre la référence, type classe
+class Field extends React.Component {
+
+    render() {
+        return <div className="form-group">
+            <label htmlFor="">{this.props.label}</label>
+            <input type="text" className="form-control" ref={this.props.forwardRef} />
+        </div>
+    }
+}
+
+// React.forwardRef permet de faire suivre la ref
+const FieldWithRef = React.forwardRef((props, ref) => {
+    // Le ...props permet de récupérer toutes les propriétés de props (ici label)
+    return <Field forwardRef={ref} {...props} />
+})
+
+// Exemple 2.1 : Pour faire descendre la référence, type fonction
+// const Field = React.forwardRef(function (props, ref) {
+//     return <div className="form-group">
+//         <input type="text" className="form-control" ref={ref}/>
+//     </div>
+// })
+
 class Home extends React.Component {
 
     constructor(props) {
@@ -18,11 +42,24 @@ class Home extends React.Component {
 
     render() {
         console.log(this.input);
+        // Pour l'exemple 2
         return <div>
-            <input type="text" ref={this.input} />
+            {/* Exemple 2.1 */}
+            {/* <Field ref={this.input}></Field> */}
+            {/* Exemple 2.2 */}
+            <FieldWithRef ref={this.input} label="demo"></FieldWithRef>
             <button onClick={this.handleClick}>Tester</button>
         </div>
     }
+
+    // Pour l'exemple 1
+    // render() {
+    //     console.log(this.input);
+    //     return <div>
+    //         <input type="text" ref={this.input} />
+    //         <button onClick={this.handleClick}>Tester</button>
+    //     </div>
+    // }
 }
 
 ReactDOM.render(<Home />, document.querySelector('#app'))
